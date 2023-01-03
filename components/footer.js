c553671a7f5ced6470f92a1a73957220f1ad96dd
Ricardo Gonzalez <ricardogzahdez@gmail.com>
2023-01-03 11:19:44 -0600

    Se agrego estilo al fotter y la seccion de nosotros se termino

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,28 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import styles from "../styles/Footer.module.css";
+
 export default function Footer() {
+  const router = useRouter();
   return (
-    <footer>
-      <p>Footer</p>
+    <footer className={styles.footer}>
+      <div className={`contenedor ${styles.contenido}`}>
+        <nav className={styles.navegacion}>
+          <Link legacyBehavior href="/">
+            Inicio
+          </Link>
+          <Link legacyBehavior href="/nosotros">
+            Nosotros
+          </Link>
+          <Link legacyBehavior href="/blog">
+            Blog
+          </Link>
+          <Link legacyBehavior href="/tienda">
+            Tienda
+          </Link>
+        </nav>
+        <p className={styles.copy}>Todos los derechos reservados {new Date().getFullYear()}</p>
+      </div>
     </footer>
   );
 }
--- components/header.js
+++ components/header.js
@@ -1,17 +1,39 @@
 import Image from "next/image";
-import Link from "next/link";
+import { useRouter } from "next/router";
 import styles from "../styles/Header.module.css";
+import Link from "next/link";
 
 export default function Header() {
+  const router = useRouter();
+  console.log(router);
   return (
     <header className={styles.header}>
       <div className={`contenedor ${styles.barra}`}>
         <Image src="/img/logo.svg" width={300} height={40} alt="imagen-logo" />
         <nav className={styles.navegacion}>
-          <Link href="/">Inicio</Link>
-          <Link href="/nosotros">Nosotros</Link>
-          <Link href="/blog">Guitarras</Link>
-          <Link href="/tienda">Contacto</Link>
+          <Link legacyBehavior href="/">
+            <a className={router?.pathname === "/" ? styles.active : ""}>
+              Inicio
+            </a>
+          </Link>
+          <Link legacyBehavior href="/nosotros">
+            <a
+              className={router?.pathname === "/nosotros" ? styles.active : ""}
+            >
+              Nosotros
+            </a>
+          </Link>
+          <Link legacyBehavior href="/tienda">
+            <a className={router?.pathname === "/tienda" ? styles.active : ""}>
+              Tienda
+            </a>
+          </Link>
+          <Link legacyBehavior href="/blog">
+            <a className={router?.pathname === "/blog" ? styles.active : ""}>
+              Blog
+            </a>
+          </Link>
+
         </nav>
       </div>
     </header>
