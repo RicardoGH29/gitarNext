057860b105d63539539d789e81824cf6d5e736af
Ricardo Gonzalez <ricardogzahdez@gmail.com>
2023-01-10 14:04:49 -0600

    empezamos a consultar api, no jala :c

---
+++ components/listadoGuitarras.js
@@ -0,0 +1,7 @@
+export default function ListadoGuitarras() {
+    return (
+        <div>
+        <h1>Listado de guitarras</h1>
+        </div>
+    );
+}
--- pages/tienda.js
+++ pages/tienda.js
@@ -1,12 +1,48 @@
 import Layout from "../components/layout";
-
+import ListadoGuitarras from "../components/listadoGuitarras";
+import {useEffect} from "react";
 export default function Tienda() {
+  useEffect(() => {
+    console.log("Tienda");
+    loadGuitarras().then(
+        (guitarras) => {
+            console.log(guitarras);
+        }
+    );
+  });
   return (
     <Layout
       title={"E-commerce"}
       description={"E-commerce de guitarras, amplificadores y accesorios"}
     >
-      <h1>Tienda</h1>
+      <main className="contenedor">
+        <h1 className='heading'>Nuestra coleccion</h1>
+        <ListadoGuitarras
+
+
+
+        />
+      </main>
     </Layout>
   );
+}
+
+export async function loadGuitarras() {
+  const res = await fetch("http://localhost:1337/api/guitarras?populate=imagen");
+  console.log(res);
+  const guitarras = await res.json();
+  console.log(guitarras);
+  return guitarras;
+}
+export async function getStaticProps() {
+  loadGuitarras().then((guitarras) => {
+    console.log(guitarras);
+  }).catch((err) => {
+    console.log("error",err);
+  });
+  return {
+    props: {
+
+    }
+  }
 }
